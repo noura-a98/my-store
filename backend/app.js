b9f95ca06e6155d07a2c0ed1459a7cf58f2ceed2
Noura <nouraaaa108@gmail.com>
2025-06-13 17:02:38 +0400

    feat(app): add morgan logger for development environment

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
 require('dotenv').config();
 
 //Enable CORS so frontend (like React) can access this API from another origin
 app.use(cors());
+
+
+
+if(process.env.NODE_ENV === 'developemt'){
+    app.use(morgan('dev')); //only log when we are in production
+}
 // Parse incoming requests with JSON payloads and make it available in req.body
 app.use(express.json());
 
