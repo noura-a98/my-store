76e54eeacd2eed8908b8f9a2fec896fa9198cdae
Noura <nouraaaa108@gmail.com>
2025-06-15 13:42:22 +0400

    feat(app) add AppError function to handle operational errors

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 require('dotenv').config();
 
 //Enable CORS so frontend (like React) can access this API from another origin
 app.use(cors());
 
 
-
-if(process.env.NODE_ENV === 'developemt'){
-    app.use(morgan('dev')); //only log when we are in production
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev')); // only log in development
 }
+
 // Parse incoming requests with JSON payloads and make it available in req.body
 app.use(express.json());
 
@@ -20,6 +22,18 @@ const userRouter = require('./routes/userRouter');
 
 // Route usage
 app.use('/api/v1/users', userRouter); 
+
+//unhandled routes
+app.use((req, res, next) => {
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`,404));
+});
+
+
+
+// global error handler
+
+app.use(globalErrorHandler);
 
 //export app
 module.exports = app;
