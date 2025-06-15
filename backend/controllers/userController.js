bb549b81e26ef501925e870b34b79e3a8276025f
Noura <nouraaaa108@gmail.com>
2025-06-15 22:51:05 +0400

    feat(controller) : add getUser function.

---
+++ backend/controllers/userController.js
@@ -0,0 +1,30 @@
+const AppError = require('../utils/appError');
+const User = require('./../models/userModel');
+const catchAsync = require ('./../utils/catchAsync');
+
+
+exports.getAllUsers= catchAsync(async (req, res, next) => {
+        const users = await User.find();
+
+        res.status(200).json({
+            status: 'success',
+            results: users.length,
+            data: {
+                users: users
+            }
+        });
+    
+});
+
+exports.getUser = catchAsync(async(req , res , next) => {
+    const user = await User.findById(req.params.id);
+    if(!user){
+        return next(new AppError('No user found with that ID.' , 404));
+    }
+    res.status(200).json({
+        status : 'sucess',
+        data:{
+            user : user
+        }
+    });
+})
