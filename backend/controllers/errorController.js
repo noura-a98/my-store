4dec1507994d0d3e1e51ab4369b8d8ff3c639362
Noura <nouraaaa108@gmail.com>
2025-06-16 13:00:18 +0400

    chore(controllers) : add handlers for validation and duplicate field DB errors.

--- backend/controllers/errorController.js
+++ backend/controllers/errorController.js
@@ -5,11 +5,21 @@ const handleCastErrorDb = err =>{
   const message = `Invaild ${err.path} : ${err.vlaue}`
   return new AppError(message ,400);
 }
+// Handle database duplication errors
+const handleDuplicateFieldsDb = err => {
+  const field = Object.keys(err.keyValue)[0];
+  const value = err.keyValue[field];
+  const message = `Duplicate field value: '${value}' for '${field}'. Please use another value.`;
+  return new AppError(message, 400);
+};
 
-// const handleDuplicateFieldsDb = err => {
-//   const value = err.errmsg.match(/(["'])(\\?.)*?\`//);
-//   const message = `Duplicate Field value : x Please use another value!.`
-// }
+// Handle validation errors 
+const handleValidationErrorDb = err =>{
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invaild input data. ${errors.join('. ')}`;
+  return new AppError(message,400);
+}
+
 const sendErrorDev = (err , res) => {
     res.status(err.statusCode).json({
     status : err.status,
@@ -50,7 +60,8 @@ module.exports = ((err, req, res, next) => {
   let error = Object.create(err);
 
     if(error.name == 'CastError') error = handleCastErrorDb(error);
-    if(error.code ==11000) error = handleDuplicateFieldsDb(error);
+    if(error.code == 11000) error = handleDuplicateFieldsDb(error);
+    if(error.name == 'ValidationError') error = handleValidationErrorDb(error);
     sendErrorProd(error,res);
 
   }  
