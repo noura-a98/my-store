a874b6621f782824a5c097b787420f5fccb625ac
Noura <nouraaaa108@gmail.com>
2025-06-17 19:54:53 +0400

    feat(controller) : add update and delete handlers.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,9 +22,38 @@ exports.getUser = catchAsync(async(req , res , next) => {
         return next(new AppError('No user found with that ID.' , 404));
     }
     res.status(200).json({
-        status : 'sucess',
+        status : 'success',
         data:{
             user : user
         }
     });
-})
+});
+
+exports.deleteUser = catchAsync(async(req , res, next) => {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if(!user){
+        return next(new AppError('No user found with that ID.', 404));
+        
+    }
+    res.status(204).json({
+        status : 'success',
+        data : null
+    })
+});
+
+exports.updateUser = catchAsync(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true, // this will return the new updated document instead of the original one
+    runValidators: true, // this will run the validators on the updated document
+  });
+  if(!user){
+    return next(new AppError('No product found with that ID', 404))
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+});
+
