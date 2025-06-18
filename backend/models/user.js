8b1ac57f3b3a9426cecb1be1ad1ba7b94f519808
Noura <nouraaaa108@gmail.com>
2025-06-18 21:22:47 +0400

    feat(models) : add a schema method to compare passwords.

--- backend/models/user.js
+++ backend/models/user.js
@@ -70,6 +70,15 @@ userSchema.pre('save', async function(next) {
 
 });
 
+userSchema.methods.correctPassword = async function (
+    candidatePassword,
+    userPassword
+){
+    return await bcrypt.compare(candidatePassword,userPassword);
+
+};
+
+
 const User = mongoose.model('User' , userSchema);
 
 module.exports = User;
