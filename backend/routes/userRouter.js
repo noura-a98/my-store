94207f44d4d814992496ef76e7ed354cdbc32cf2
Noura <nouraaaa108@gmail.com>
2025-06-20 17:45:35 +0400

    feat(routes) : add restrictTo middleware to the delete route.

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -8,7 +8,7 @@ router.get('/',authController.protect, userController.getAllUsers);
 router.post('/createAccount', authController.createAccount);
 router.post('/login',authController.login);
 router.get('/:id', userController.getUser);
-router.delete('/:id', userController.deleteUser);
+router.delete('/:id',authController.protect,authController.restrictTo('admin'), userController.deleteUser);
 router.patch('/:id', userController.updateUser);
 
 module.exports = router;
