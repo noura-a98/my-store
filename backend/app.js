af2abda6e09168716437d6255c051596c6e8261e
Noura <nouraaaa108@gmail.com>
2025-06-20 22:50:37 +0400

    feat(app) : add rate limiter to limit the number of requests commint from a single IP.

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,43 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const app = express();
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 require('dotenv').config();
+
+const app = express();
+
+// Set security HTTP headers
+app.use(helmet());
+
 
 //Enable CORS so frontend (like React) can access this API from another origin
 app.use(cors());
 
 
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); // only log in development
 }
 
-// Parse incoming requests with JSON payloads and make it available in req.body
-app.use(express.json());
+// limit requests from same IP
+const limiter = rateLimit({
+  max:150,
+  windowMs: 60 *60 * 1000,
+  message : 'Too many requests from this IP, please try again in an hour!'
+});
+
+app.use('/api',limiter)
+
+// Body parser , reading data from body into req.body
+app.use(express.json({limit:'10kb'}));
+
+// Data sanitization against NoSQL query injection
+
+
+
 
 // Route import
 const userRouter = require('./routes/userRouter');
