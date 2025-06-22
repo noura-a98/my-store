29da5dcf105b33c779b1f93a6741855a32d79639
Noura <nouraaaa108@gmail.com>
2025-06-22 21:27:08 +0400

    feat(app) : add data sanitization middlewares like express-mongo-sanitize and xss-clean but they are giving me errors.

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const rateLimit = require('express-rate-limit')
 const helmet = require('helmet');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 require('dotenv').config();
 
 const app = express();
@@ -35,7 +37,11 @@ app.use('/api',limiter)
 app.use(express.json({limit:'10kb'}));
 
 // Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
 
+// Data protection against xss
+
+app.use(xss());
 
 
 
