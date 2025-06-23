1ef00870e8e4290fda3194eae5de8f30565ce96f
Noura <nouraaaa108@gmail.com>
2025-06-23 21:56:34 +0400

    feat(controllers) : add handler factory logic.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const AppError = require('../utils/appError');
 const User = require('../models/user');
 const catchAsync = require ('./../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 
 // const filterObj = (obj , ...allowedFields)=> {
@@ -33,6 +34,10 @@ const catchAsync = require ('./../utils/catchAsync');
 
   
 // });
+exports.getAllUsers = factory.getAllOne(User);
+exports.deleteUser = factory.deleteOne(User);
+exports.updateUser = factory.updateOne(User);
+exports.getUser = factory.getOne(User);
 
 exports.deleteMe = catchAsync(async (req, res, next) => {
          await User.findByIdAndUpdate(req.user.id , {active : false});
@@ -43,57 +48,58 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
         });
     
 });
-exports.getAllUsers= catchAsync(async (req, res, next) => {
-        const users = await User.find();
 
-        res.status(200).json({
-            status: 'success',
-            results: users.length,
-            data: {
-                users: users
-            }
-        });
+// exports.getAllUsers= catchAsync(async (req, res, next) => {
+//         const users = await User.find();
+
+//         res.status(200).json({
+//             status: 'success',
+//             results: users.length,
+//             data: {
+//                 users: users
+//             }
+//         });
     
-});
+// });
 
-exports.getUser = catchAsync(async(req , res , next) => {
-    const user = await User.findById(req.params.id);
-    if(!user){
-        return next(new AppError('No user found with that ID.' , 404));
-    }
-    res.status(200).json({
-        status : 'success',
-        data:{
-            user : user
-        }
-    });
-});
+// exports.getUser = catchAsync(async(req , res , next) => {
+//     const user = await User.findById(req.params.id);
+//     if(!user){
+//         return next(new AppError('No user found with that ID.' , 404));
+//     }
+//     res.status(200).json({
+//         status : 'success',
+//         data:{
+//             user : user
+//         }
+//     });
+// });
 
-exports.deleteUser = catchAsync(async(req , res, next) => {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if(!user){
-        return next(new AppError('No user found with that ID.', 404));
+// exports.deleteUser = catchAsync(async(req , res, next) => {
+//     const user = await User.findByIdAndDelete(req.params.id);
+//     if(!user){
+//         return next(new AppError('No user found with that ID.', 404));
         
-    }
-    res.status(204).json({
-        status : 'success',
-        data : null
-    })
-});
+//     }
+//     res.status(204).json({
+//         status : 'success',
+//         data : null
+//     })
+// });
 
-exports.updateUser = catchAsync(async (req, res, next) => {
-  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-    new: true, // this will return the new updated document instead of the original one
-    runValidators: true, // this will run the validators on the updated document
-  });
-  if(!user){
-    return next(new AppError('No user found with that ID', 404))
-  }
+// exports.updateUser = catchAsync(async (req, res, next) => {
+//   const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+//     new: true, // this will return the new updated document instead of the original one
+//     runValidators: true, // this will run the validators on the updated document
+//   });
+//   if(!user){
+//     return next(new AppError('No user found with that ID', 404))
+//   }
 
-  res.status(200).json({
-    status: 'success',
-    message: 'user updated successfully.'
+//   res.status(200).json({
+//     status: 'success',
+//     message: 'user updated successfully.'
 
-  });
-});
+//   });
+// });
 
