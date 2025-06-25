1b6961650faab5d30e2132f8a0029701fdda75f7
Noura <nouraaaa108@gmail.com>
2025-06-25 23:08:20 +0400

    feat(routes) : add getCheckoutSession api.

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -3,22 +3,24 @@ const router = express.Router();
 const orderController = require('../controllers/orderController');
 const authController = require('../controllers/authController');
 
-// Public: Create order
-router.post('/', orderController.createOrder);
+router.post('/checkoutSession/:productId',authController.protect,orderController.getCheckoutSession
+)
+// // Public: Create order
+// router.post('/', orderController.createOrder);
 
-// Protect all routes below
-router.use(authController.protect);
+// // Protect all routes below
+// router.use(authController.protect);
 
-// Admin or driver can view all orders
-router.get('/', authController.restrictTo('admin', 'driver'), orderController.getAllOrders);
+// // Admin or driver can view all orders
+// router.get('/', authController.restrictTo('admin', 'driver'), orderController.getAllOrders);
 
-// Get specific order by ID
-router.get('/:id', authController.restrictTo('admin', 'driver'), orderController.getOrder);
+// // Get specific order by ID
+// router.get('/:id', authController.restrictTo('admin', 'driver'), orderController.getOrder);
 
-// Update order status (admin or driver)
-router.patch('/:id/status', authController.restrictTo('admin', 'driver'), orderController.updateOrderStatus);
+// // Update order status (admin or driver)
+// router.patch('/:id/status', authController.restrictTo('admin', 'driver'), orderController.updateOrderStatus);
 
-// Assign driver (admin only)
-router.patch('/:id/assign-driver', authController.restrictTo('admin'), orderController.assignDriver);
+// // Assign driver (admin only)
+// router.patch('/:id/assign-driver', authController.restrictTo('admin'), orderController.assignDriver);
 
 module.exports = router;
