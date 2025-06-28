59e5ae9a21a6f4e52b27112236d82b4a1013f18e
Noura <nouraaaa108@gmail.com>
2025-06-28 20:05:47 +0400

    chore(models) : remove the required field in imageCover.

--- backend/models/product.js
+++ backend/models/product.js
@@ -28,7 +28,7 @@ const productSchema = new mongoose.Schema({
     images: [String],
     imageCover : {
         type : String,
-        required : [true, 'Image cover is required.']
+        
     },
     createdAt : {
         type : Date,
