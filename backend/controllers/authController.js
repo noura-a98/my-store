95e7f57c996680448576e9ea719ea1415785f343
Noura <nouraaaa108@gmail.com>
2025-11-09 19:29:07 +0400

    fix(controllers): fix the login functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -156,3 +156,10 @@ exports.updateUsersPassword = catchAsync(async (req, res, next) => {
 });
 
 
+exports.logout = (req, res, next) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({ status: 'success' });
+};
