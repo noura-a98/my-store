a2329f7a76a5d5c909fad83d9c16396f0d5aaa4d
Noura <nouraaaa108@gmail.com>
2025-11-09 19:42:32 +0400

    chore(models) : add the validator package.

--- backend/models/influencer.js
+++ backend/models/influencer.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const InfluencerSchema = new mongoose.Schema({
   name: {
@@ -12,6 +13,30 @@ const InfluencerSchema = new mongoose.Schema({
     lowercase: true, 
     trim: true
   },
+  email: {
+    type: String,
+    required: [true, 'Email is required!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email.']
+  },
+  phone: {
+    type: String,
+    required: [true, 'Phone number is required!'],
+    trim: true
+  },
+  instagram: {
+    type: String,
+    trim: true
+  },
+  tiktok: {
+    type: String,
+    trim: true
+  },
+  active: {
+    type: Boolean,
+    default: true
+  },
   createdAt: {
     type: Date,
     default: Date.now
@@ -19,5 +44,4 @@ const InfluencerSchema = new mongoose.Schema({
 });
 
 const Influencer = mongoose.model('Influencer', InfluencerSchema);
-
 module.exports = Influencer;
