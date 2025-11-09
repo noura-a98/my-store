9b1e2422ddbd622f4dd5f7d60ac9a9b2f29d5316
Noura <nouraaaa108@gmail.com>
2025-11-09 19:41:01 +0400

    feat(controllers) : add the getMe function to return the current user.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,10 @@ exports.getAllUsers = factory.getAllOne(User);
 exports.deleteUser = factory.deleteOne(User);
 exports.updateUser = factory.updateOne(User);
 exports.getUser = factory.getOne(User);
-
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
 exports.deleteMe = catchAsync(async (req, res, next) => {
          await User.findByIdAndUpdate(req.user.id , {active : false});
 
