dffdbb5bf7f672f974bc521684e01bfed3d6c87d
Noura <nouraaaa108@gmail.com>
2026-09-16 14:21:29 +0400

    fix the origin

--- backend/app.js
+++ backend/app.js
@@ -1,53 +1,54 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit')
-const helmet = require('helmet');
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const path =require('path')
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const path = require("path");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
 // Set security HTTP headers
 app.use(helmet());
 
-
 //Enable CORS so frontend (like React) can access this API from another origin
-app.use(cors({
-  origin: 'http://localhost:3000', // or your frontend URL
-  credentials: true
-}));
+// app.use(cors({
+//   origin: 'http://localhost:3000', // or your frontend URL
+//   credentials: true
+// }));
+app.use(cors({ origin: "https://sweetdrop.nouradev.com" }));
+
 // Add this middleware before your static file serving
-app.use('/img', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', 'GET');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+app.use("/img", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
-app.use('/img', express.static(path.join(__dirname, 'public/img')));
+app.use("/img", express.static(path.join(__dirname, "public/img")));
 
 // Development logging
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev')); // only log in development
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev")); // only log in development
 }
 
 // limit requests from same IP
 const limiter = rateLimit({
-  max:1000,
-  windowMs: 60 *60 * 1000,
-  message : 'Too many requests from this IP, please try again in an hour!'
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
 });
 
-app.use('/api',limiter)
+app.use("/api", limiter);
 
 // Body parser , reading data from body into req.body
-app.use(express.json({limit:'10kb'}));
+app.use(express.json({ limit: "10kb" }));
 // Data sanitization against NoSQL query injection
 // app.use(mongoSanitize());
 
@@ -55,32 +56,27 @@ app.use(express.json({limit:'10kb'}));
 
 // app.use(xss());
 
-
-
 // Route import
-const userRouter = require('./routes/userRouter');
-const productRouter = require('./routes/productRouter')
-const influencerRouter = require('./routes/influencerRouter');
-const orderRouter = require('./routes/orderRouter');
+const userRouter = require("./routes/userRouter");
+const productRouter = require("./routes/productRouter");
+const influencerRouter = require("./routes/influencerRouter");
+const orderRouter = require("./routes/orderRouter");
 // const webhookRoute = require('./routes/webhookRoutes');
-const deliveryFee = require('./routes/deliveryFeesRouter');
-const contactRouter = require('./routes/contactRouter');
+const deliveryFee = require("./routes/deliveryFeesRouter");
+const contactRouter = require("./routes/contactRouter");
 // Route usage
-app.use('/api/v1/users', userRouter); 
-app.use('/api/v1/products',productRouter);
-app.use('/api/v1/influencers',influencerRouter);
-app.use('/api/v1/orders',orderRouter);
-app.use('/api/v1/deliveryFee', deliveryFee);
-app.use('/api/v1/contact',contactRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/influencers", influencerRouter);
+app.use("/api/v1/orders", orderRouter);
+app.use("/api/v1/deliveryFee", deliveryFee);
+app.use("/api/v1/contact", contactRouter);
 // app.use('/api/v1', webhookRoute);
 
 //unhandled routes
 app.use((req, res, next) => {
-
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`,404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
-
-
 
 // add global error handler
 app.use(globalErrorHandler);
